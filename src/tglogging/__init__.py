417a4d6e4473068fec7dcb62213d079bf6a05571
licryle <cyrille@berliat.fr>
2026-05-23 17:31:39 +0800

    For clarity and earlier access to PRIORITY_INFO before monkey patch is applied, explicitly export PRIORITY_INFO.

--- src/tglogging/__init__.py
+++ src/tglogging/__init__.py
@@ -1,8 +1,8 @@
 from .tglogging import configure_logger, LoggingConfig
-
 from .priority_info import PRIORITY_INFO
 
 __all__ = [
     "configure_logger",
     "LoggingConfig",
+    "PRIORITY_INFO",
 ]
